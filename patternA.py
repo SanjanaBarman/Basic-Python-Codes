f81276c165c86730900cf8462ae1d4ce2a72cd2c
Sanjana Barman <142417943+SanjanaBarman@users.noreply.github.com>
2024-02-04 20:44:03 +0530

    Update patternA.py

--- patternA.py
+++ patternA.py
@@ -6,3 +6,17 @@ for i in range(n):
         print(chr(ch), end = " ")
         ch = ch + 1
     print()
+
+
+
+'''Enter the number of rows:10
+A 
+A B 
+A B C 
+A B C D 
+A B C D E 
+A B C D E F 
+A B C D E F G 
+A B C D E F G H 
+A B C D E F G H I 
+A B C D E F G H I J '''
