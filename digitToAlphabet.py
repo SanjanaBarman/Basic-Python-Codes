2771cac826fafa6656d29e515a473aeb63905f79
Sanjana Barman <142417943+SanjanaBarman@users.noreply.github.com>
2024-02-05 23:01:27 +0530

    Add files via upload

---
+++ digitToAlphabet.py
@@ -0,0 +1,7 @@
+dict={1:"ONE",2:"TWO",3:"THREE",4:"FOUR",5:"FIVE",6:"SIX",7:"SEVEN",8:"EIGHT",9:"NINE",10:"TEN"}
+n=int(input("Enter the number between 1 to 10:"))
+print(n,"=",dict[n])
+    
+
+'''Enter the number between 1 to 10:10
+10 = TEN'''
